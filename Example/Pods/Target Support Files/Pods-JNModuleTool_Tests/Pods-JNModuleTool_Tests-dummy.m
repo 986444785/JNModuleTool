28958be44874f251828dd8c6cf6b793ec86b7047
Evon2019 <986444785@qq.com>
2020-05-19 15:47:33 +0800

    组件工程

---
+++ Example/Pods/Target Support Files/Pods-JNModuleTool_Tests/Pods-JNModuleTool_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JNModuleTool_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_JNModuleTool_Tests
+@end
---
+++ JNModuleTool/Classes/JNPeople.m
@@ -0,0 +1,12 @@
+//
+//  JNPeople.m
+//  Expecta
+//
+//  Created by Evon2019 on 2020/5/19.
+//
+
+#import "JNPeople.h"
+
+@implementation JNPeople
+
+@end
