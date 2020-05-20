27b1dd048a5449921ca450aaf8fc857edf00c98b
Evon2019 <986444785@qq.com>
2020-05-20 15:59:37 +0800

    添加控件

--- Example/Pods/Target Support Files/JNModuleTool/JNModuleTool-umbrella.h
+++ Example/Pods/Target Support Files/JNModuleTool/JNModuleTool-umbrella.h
@@ -10,7 +10,7 @@
 #endif
 #endif
 
-#import "JNPeople.h"
+#import "SegmentTitleView.h"
 
 FOUNDATION_EXPORT double JNModuleToolVersionNumber;
 FOUNDATION_EXPORT const unsigned char JNModuleToolVersionString[];
